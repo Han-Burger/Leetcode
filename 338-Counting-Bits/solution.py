661525158deaab5096bbb3f08813bb9763a7e950
Han Hong <honghan.vip@gmail.com>
2016-08-20 17:55:43 -0400

    [338. Counting Bits][Accepted]committed by LeetCode Extension

--- 338-Counting-Bits/solution.py
+++ 338-Counting-Bits/solution.py
@@ -1,10 +1,25 @@
-
 class Solution(object):
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
         """
+        return self.noBitManipulation(num)
+    
+    def noBitManipulation(self, num):
+        if num == 0: return [0]
+        
+        m = [0, 1] + [0] * (num - 1)
+        prev2Exp = 1
+        for i in xrange(2, len(m)):
+            if i == prev2Exp * 2:
+                m[i] = 1
+                prev2Exp *= 2
+            else:
+                m[i] = 1 + m[i - prev2Exp]
+        return m
+        
+    def bitManipulation(self, num):
         m = [0] * (num + 1)
         scale = 0
         for i in xrange(1, len(m)):
