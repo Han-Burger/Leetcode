002dde6572c1417cf6a2077d929ed6a5a4c54c4c
Han Hong <honghan.vip@gmail.com>
2016-09-03 18:12:31 -0400

    [18. 4Sum][Accepted]committed by LeetCode Extension

--- 18-4Sum/solution.py
+++ 18-4Sum/solution.py
@@ -15,6 +15,9 @@ class Solution(object):
         
         d = set()
         length = len(nums)
+        
+        print nums
+        
         for i in xrange(length):
             # should not skip duplicate numbers when traversing the list for the first number
             # e.g., [1, 1, 1, 1, 2] and is looking for a target of 5.
@@ -22,17 +25,21 @@ class Solution(object):
             for j in xrange(i + 1, length):
                 subTarget = target - nums[i] - nums[j]
                 p, q = j + 1, length - 1
-                while p < q:
-                    if nums[p] + nums[q] == subTarget:
-                        d.add((nums[i], nums[j], nums[p], nums[q]))
-                        p += 1
-                        q -= 1
-                        while p < q and nums[p] == nums[p - 1]: p += 1
-                        while p < q and nums[q] == nums[q + 1]: q -= 1
-                    elif nums[p] + nums[q] < subTarget:
-                        p += 1
-                        while p < q and nums[p] == nums[p - 1]: p += 1
-                    else:
-                        q -= 1
-                        while p < q and nums[q] == nums[q + 1]: q -= 1
+                ### NOTE: wipe out some uncessarily recursion by pre-sum check
+                if p >= q or subTarget < nums[p] * 2 or subTarget > nums[q] * 2:
+                    continue
+                else:
+                    while p < q:
+                        if nums[p] + nums[q] == subTarget:
+                            d.add((nums[i], nums[j], nums[p], nums[q]))
+                            p += 1
+                            q -= 1
+                            while p < q and nums[p] == nums[p - 1]: p += 1
+                            while p < q and nums[q] == nums[q + 1]: q -= 1
+                        elif nums[p] + nums[q] < subTarget:
+                            p += 1
+                            while p < q and nums[p] == nums[p - 1]: p += 1
+                        else:
+                            q -= 1
+                            while p < q and nums[q] == nums[q + 1]: q -= 1
         return list(d)
