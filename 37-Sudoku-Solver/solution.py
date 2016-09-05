8b51eacfc7521051673d60e9a258962cc5e1aa32
Han Hong <honghan.vip@gmail.com>
2016-09-05 16:47:10 -0400

    [37. Sudoku Solver][Accepted]committed by LeetCode Extension

---
+++ 37-Sudoku-Solver/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def solveSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
+        self.solve(board)
+    
+    def backtracking(self, b):
+        self.solve(b)
+    
+    def solve(self, b):
+        for i in xrange(9):
+            for j in xrange(9):
+                if b[i][j] == '.':
+                    for c in map(str, xrange(1, 10)):
+                        if self.valid(b, i, j, c):
+                            b[i][j] = c
+                            if self.solve(b): return True
+                            else: b[i][j] = '.'
+                    # has no solution for this cell
+                    return False
+        return True
+                            
+    
+    def valid(self, b, p, q, c):
+        # b for board, p for row, q for column, c for input
+        for j in xrange(9):
+            if j != q and b[p][j] == c: return False
+        for i in xrange(9):
+            if i != p and b[i][q] == c: return False
+        for i in xrange(3):
+            for j in xrange(3):
+                row, col = (p / 3) * 3 + i, (q / 3) * 3 + j
+                if row != p and col != q and b[row][col] == c: return False
+        return True
+                    
