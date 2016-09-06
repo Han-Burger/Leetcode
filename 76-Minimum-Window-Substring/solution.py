4226558901f23648ac4ad6442e6bc8fc87ceceb9
Han Hong <honghan.vip@gmail.com>
2016-09-05 20:57:00 -0400

    [76. Minimum Window Substring][Accepted]committed by LeetCode Extension

--- 76-Minimum-Window-Substring/solution.py
+++ 76-Minimum-Window-Substring/solution.py
@@ -1,11 +1,43 @@
 class Solution(object):
     def minWindow(self, s, t):
-        from collections import deque
         """
         :type s: str
         :type t: str
         :rtype: str
         """
+        return self.mapWith2Ptrs(s, t)
+        
+    def mapWith2Ptrs(self, s, t):
+        if not t or len(s) < len(t): return ""
+        
+        w_count = {}
+        for c in t:
+            if c in w_count: w_count[c] += 1
+            else: w_count[c] = 1
+        
+        head, tail, count = 0, 0, len(t)
+        min_start, min_len = 0, len(s) + 1
+        while tail < len(s):
+            if s[tail] in w_count:
+                if w_count[s[tail]] > 0:
+                    count -= 1
+                w_count[s[tail]] -= 1
+            tail += 1
+            while count == 0:
+                l = tail - head
+                if l < min_len:
+                    min_start = head
+                    min_len = l
+                if s[head] in w_count:
+                    w_count[s[head]] += 1
+                    if w_count[s[head]] > 0:
+                        count += 1
+                head += 1
+        return "" if min_len > len(s) else s[min_start: min_start + min_len]
+        
+    def mapWithQueue(self, s, t):
+        from collections import deque
+        
         if not t or len(s) < len(t): return ""
         
         #w_count counts the frequency of char in t
@@ -13,7 +45,6 @@ class Solution(object):
         for c in t:
             if c in w_count: w_count[c] += 1
             else: w_count[c] = 1
-        print w_count
         m = {}
         min_str = s
         min_pos = -1
