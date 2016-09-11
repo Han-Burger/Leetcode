a490f9e7506bf9fe3f51ee2fd4caffd5ac29f733
Han Hong <honghan.vip@gmail.com>
2016-09-11 15:49:32 -0400

    [177. Nth Highest Salary][Accepted]committed by LeetCode Extension

---
+++ 177-Nth-Highest-Salary/solution.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select e1.Salary
+      from (select distinct Salary from Employee) e1
+      where (select count(*) from (select distinct Salary from Employee) e2 where e2.Salary > e1.Salary) = N-1
+  );
+END
