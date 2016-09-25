529e436668eef98cedad44f45fc986caa595575c
Han Hong <honghan.vip@gmail.com>
2016-09-25 01:35:47 -0400

    [33. Search in Rotated Sorted Array][Accepted]committed by LeetCode Extension

---
+++ 33-Search-in-Rotated-Sorted-Array/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        if not nums: return -1
+        if len(nums) < 3:
+            for i in xrange(len(nums)):
+                if target == nums[i]: return i
+            return -1
+        
+        # at least 2 values
+        i, j = 0, len(nums) - 1
+        leftVal, rightVal = nums[i], nums[j]
+        if leftVal == target: return i
+        if rightVal == target: return j
+        
+        while i < j - 1:
+            mid = i + (j - i) / 2
+            if nums[mid] == target: return mid
+            if leftVal < rightVal:
+                if nums[mid] < target: i = mid
+                else: j = mid
+            elif target > leftVal:
+                if nums[mid] < rightVal or nums[mid] > target: j = mid
+                else: i = mid
+            else:
+                if nums[mid] > leftVal or nums[mid] < target: i = mid
+                else: j = mid
+        return -1
+        
